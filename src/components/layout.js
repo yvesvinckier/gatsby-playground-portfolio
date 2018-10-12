0cf08ed0dfd8b74fbcf30cc83546be01d1cbde10
Yves Vinckier <yves.vinckier@live.fr>
2018-10-12 22:46:12 +0200

    css-hover-mask

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,8 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+import '../stylesheets/style.scss'
+// import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
---
+++ src/pages/css-hover-mask.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Title from '../components/Title'
+
+class Mask extends Component {
+    render() {
+        return (
+            <div>
+                <Title text="CSS HOVER MASK" />
+            </div>
+        )
+    }
+}
+
+export default Mask
